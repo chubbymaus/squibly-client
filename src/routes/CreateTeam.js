f327f64bec19ada894f04a04a066f04960bd2bd2
chubbymaus <court@chubbymaus.com>
2018-07-20 22:32:28 -0600

    added createTeam component and mutation

---
+++ src/routes/CreateTeam.js
@@ -0,0 +1,94 @@
+import React, { Component } from "react";
+import { extendObservable } from "mobx";
+import { observer } from "mobx-react";
+import { Form, Message, Container, Header, Input, Button } from "semantic-ui-react";
+import { graphql } from "react-apollo";
+import gql from "graphql-tag";
+
+class CreateTeam extends Component {
+    constructor(props) {
+        super(props);
+
+        extendObservable(this, {
+            name: "",
+            errors: {},
+        });
+    }
+
+    onSubmit = async () => {
+        const { name } = this;
+        const response = await this.props.mutate({
+            variables: { name }
+        });
+
+        console.log(response);
+
+        const { ok, errors } = response.data.createTeam;
+
+        if (ok) {
+            this.props.history.push("/");
+        } else {
+            const err = {};
+            errors.forEach(({ path, message }) => {
+                // err['passwordError'] = 'too long..';
+                err[`${path}Error`] = message;
+            });
+
+            this.errors = err;
+        }
+    };
+
+    onChange = (e) => {
+        const { name, value } = e.target;
+        this[name] = value;
+    };
+
+    render() {
+        const { name, errors: { nameError, } } = this;
+
+        const errorList = [];
+
+        if (nameError) {
+            errorList.push(nameError);
+        }
+
+        return (
+            <Container text>
+                <Header as="h2">Create a Team</Header>
+                <Form>
+                    <Form.Field error={!!nameError}>
+                        <Input
+                            name="name"
+                            onChange={this.onChange}
+                            value={name}
+                            placeholder="Team Name"
+                            fluid
+                        />
+                    </Form.Field>
+                    <Button onClick={this.onSubmit}>Submit</Button>
+                </Form>
+                {errorList.length ? (
+                    <Message
+                        error
+                        header="There was some errors with your submission"
+                        list={errorList}
+                    />
+                ) : null}
+            </Container>
+        );
+    }
+}
+
+const createTeamMutation = gql`
+  mutation($name: String!) {
+    createTeam(name: $name) {
+      ok
+      errors {
+        path
+        message
+      }
+    }
+  }
+`;
+
+export default graphql(createTeamMutation)(observer(CreateTeam));
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './Home';
 import Register from './Register';
 import Login from './Login';
+import CreateTeam from './CreateTeam';
 
 const Routes = () => (
   <BrowserRouter>
@@ -11,6 +12,7 @@ const Routes = () => (
       <Route path="/" exact component={Home} />
       <Route path="/register" exact component={Register} />
       <Route path="/login" exact component={Login} />
+      <Route path="/create-team" exact component={CreateTeam} />
     </Switch>
   </BrowserRouter>
 );
