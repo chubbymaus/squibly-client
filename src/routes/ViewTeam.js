ae259ef00c760e97ce4d88dad8ea9eeaf6d2948b
chubbymaus <court@loftydev.com>
2018-07-28 16:05:08 -0600

    refactored owner out of teams into members as admin

--- src/routes/ViewTeam.js
+++ src/routes/ViewTeam.js
@@ -7,43 +7,46 @@ import Header from '../components/Header';
 import SendMessage from '../components/SendMessage';
 import AppLayout from '../components/AppLayout';
 import Sidebar from '../containers/Sidebar';
-import { allTeamsQuery } from '../graphql/team';
 import MessageContainer from '../containers/MessageContainer';
+import { meQuery } from '../graphql/team';
 
-const ViewTeam = ({ data: { loading, allTeams, inviteTeams }, match: { params: { teamId, channelId } } }) => {
-    if (loading) {
-        return null;
-    }
+const ViewTeam = ({
+  data: { loading, me, ...otherProps },
+  match: { params: { teamId, channelId } },
+}) => {
+  if (loading) {
+    return null;
+  }
+  console.log(otherProps);
+  console.log(me);
+  const { teams } = me;
 
-    const teams = [...allTeams, ...inviteTeams];
+  if (!teams.length) {
+    return <Redirect to="/create-team" />;
+  }
 
-    if (!teams.length) {
-        return <Redirect to="/create-team" />;
-    }
+  const teamIdInteger = parseInt(teamId, 10);
+  const teamIdx = teamIdInteger ? findIndex(teams, ['id', teamIdInteger]) : 0;
+  const team = teamIdx === -1 ? teams[0] : teams[teamIdx];
 
-    const teamIdInteger = parseInt(teamId, 10);
+  const channelIdInteger = parseInt(channelId, 10);
+  const channelIdx = channelIdInteger ? findIndex(team.channels, ['id', channelIdInteger]) : 0;
+  const channel = channelIdx === -1 ? team.channels[0] : team.channels[channelIdx];
 
-    const teamIdx = teamIdInteger ? findIndex(teams, ['id', teamIdInteger]) : 0;
-    const team = teamIdx === -1 ? teams[0] : teams[teamIdx];
-
-    const channelIdInteger = parseInt(channelId, 10);
-    const channelIdx = channelId ? findIndex(team.channels, ['id', channelIdInteger]) : 0;
-    const channel = channelIdx === -1 ? team.channels[0] : team.channels[channelIdx];
-
-    return (
-        <AppLayout>
-            <Sidebar
-                teams={teams.map(t => ({
-                    id: t.id,
-                    letter: t.name.charAt(0).toUpperCase(),
-                }))}
-                team={team}
-            />
-            {channel && <Header channelName={channel.name} />}
-            {channel && <MessageContainer channelId={channel.id} />}
-            {channel && <SendMessage channelName={channel.name} channelId={channel.id} />}
-        </AppLayout>
-    );
+  return (
+    <AppLayout>
+      <Sidebar
+        teams={teams.map(t => ({
+          id: t.id,
+          letter: t.name.charAt(0).toUpperCase(),
+        }))}
+        team={team}
+      />
+      {channel && <Header channelName={channel.name} />}
+      {channel && <MessageContainer channelId={channel.id} />}
+      {channel && <SendMessage channelName={channel.name} channelId={channel.id} />}
+    </AppLayout>
+  );
 };
 
-export default graphql(allTeamsQuery)(ViewTeam);
+export default graphql(meQuery, { options: { fetchPolicy: 'network-only' } })(ViewTeam);
